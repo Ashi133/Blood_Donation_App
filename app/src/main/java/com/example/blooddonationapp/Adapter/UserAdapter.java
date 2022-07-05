16274feba247e56b8ff884412a2b9d981ef02868
Ashi <ashiagrawal133@gmail.com>
2022-07-05 22:20:12 +0530

    Blood_Donation-0.0.0

---
+++ app/src/main/java/com/example/blooddonationapp/Adapter/UserAdapter.java
@@ -0,0 +1,77 @@
+package com.example.blooddonationapp.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.blooddonationapp.Model.User;
+import com.example.blooddonationapp.R;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class UserAdapter  extends  RecyclerView.Adapter<UserAdapter.ViewHolder>{
+
+private Context context;
+private List<User> userList;
+
+    public UserAdapter(List<User> userList) {
+        this.userList = userList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate
+                (R.layout.user_displayed_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+     final User user = userList.get(position);
+
+        holder.type.setText(user.getType());
+        holder.userEmail.setText(user.getType());
+        holder.phoneNumber.setText(user.getType());
+        holder.username.setText(user.getType());
+        holder.bloodGroup.setText(user.getType());
+
+        Glide.with(context).load(user.getProfileimagepocture()).into(holder.userProfileImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+             public CircleImageView userProfileImage;
+             public TextView type, username, userEmail, phoneNumber, bloodGroup;
+             public Button emailNow;
+
+             public ViewHolder(@NonNull View itemView) {
+                 super(itemView);
+
+                 userProfileImage = itemView.findViewById(R.id.userProfileImage);
+                 type = itemView.findViewById(R.id.type);
+                 username = itemView.findViewById(R.id.userName);
+                 userEmail = itemView.findViewById(R.id.userEmail);
+                 phoneNumber = itemView.findViewById(R.id.phoneNumber);
+                 bloodGroup = itemView.findViewById(R.id.bloodGroup);
+                 emailNow = itemView.findViewById(R.id.emailNow);
+
+             }
+
+         }
+}
---
+++ app/src/main/java/com/example/blooddonationapp/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.blooddonationapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener {
+    
+    private DrawerLayout drawerLayout;
+    private NavigationView nav_view;
+    private DrawerLayout.DrawerListener toggle;
+    private Toolbar toolbar;
+
+    private CircleImageView nav_profile_image;
+    private TextView nav_fullname, nav_email, nav_bloodgroup, nav_type;
+
+    private DatabaseReference userRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        getSupportActionBar().setTitle("Blood Donation App");
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        nav_view=findViewById(R.id.nav_view);
+        
+        ActionBarDrawerToggle  actionBarDrawerToggle= new ActionBarDrawerToggle(MainActivity.this, drawerLayout,
+                toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        actionBarDrawerToggle.syncState();
+
+        nav_view.setNavigationItemSelectedListener(this);
+
+        nav_profile_image = nav_view.getHeaderView(0).findViewById(R.id.nav_user_image);
+        nav_fullname = nav_view.getHeaderView(0).findViewById(R.id.nav_user_fullname);
+        nav_bloodgroup = nav_view.getHeaderView(0).findViewById(R.id.nav_user_bloodgroup);
+        nav_email = nav_view.getHeaderView(0).findViewById(R.id.nav_user_email);
+        nav_type = nav_view.getHeaderView(0).findViewById(R.id.nav_user_type);
+
+        userRef = FirebaseDatabase.getInstance().getReference().child("user").child(
+                FirebaseAuth.getInstance().getCurrentUser().getUid()
+        );
+
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    String name = snapshot.child("name").getValue().toString();
+                    nav_fullname.setText(name);
+
+                    String email = snapshot.child("email").getValue().toString();
+                    nav_email.setText(email);
+
+                    String bloodgroup = snapshot.child("bloodgroup").getValue().toString();
+                    nav_bloodgroup.setText(bloodgroup);
+
+                    String type = snapshot.child("type").getValue().toString();
+                    nav_type.setText(type);
+
+                    if (snapshot.hasChild("profileimage")) {
+                        String imageUrl = snapshot.child("profilepictureurl").getValue().toString();
+                        Glide.with(getApplicationContext()).load(imageUrl).into(nav_profile_image);
+
+                    } else {
+                        nav_profile_image.setImageResource(R.drawable.profile_image);
+                    }
+                    }
+                }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.profile:
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
